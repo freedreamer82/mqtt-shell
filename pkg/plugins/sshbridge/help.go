ce6cacceea554d158a25f9e9de2087090cd04be6
Marco Garzola <mgarzola@tecniplast.it>
2025-05-29 14:36:41 +0200

    fix help formatting

--- pkg/plugins/sshbridge/help.go
+++ pkg/plugins/sshbridge/help.go
@@ -2,15 +2,15 @@ package sshbridge
 
 import "strings"
 
-const helpText = "%s <user@host> [password] [-p port] [-i keyfile] [--raw]\n" +
+const helpText = "*** <user@host> [password] [-p port] [-i keyfile] [--raw]\n" +
 	"- Connect via SSH. Options:\n" +
 	"  -p <port>     : specify port (default 22)\n" +
 	"  -i <keyfile>  : use private key authentication\n" +
 	"  --raw         : enable raw mode (interactive shell, line-by-line reading)\n" +
 	"Examples:\n" +
-	"  %s user@host password\n" +
-	"  %s user@host -i /path/to/keyfile --raw\n" +
-	"  %s user@host password -p 2222 --raw"
+	"  *** user@host password\n" +
+	"  *** user@host -i /path/to/keyfile --raw\n" +
+	"  *** user@host password -p 2222 --raw"
 
 const errorText = "***: command not valid, try > *** help"
 
