3b19c78cca5b564a21daad9b6a3a9ed1b85deb25
Marco Garzola <mgarzola@tecniplast.it>
2025-05-29 14:40:03 +0200

    fix help adding disconnect

--- pkg/plugins/sshbridge/help.go
+++ pkg/plugins/sshbridge/help.go
@@ -7,6 +7,7 @@ const helpText = "*** <user@host> [password] [-p port] [-i keyfile] [--raw]\n" +
 	"  -p <port>     : specify port (default 22)\n" +
 	"  -i <keyfile>  : use private key authentication\n" +
 	"  --raw         : enable raw mode (interactive shell, line-by-line reading)\n" +
+	"  *** disconnect -> close ssh connection\n" +
 	"Examples:\n" +
 	"  *** user@host password\n" +
 	"  *** user@host -i /path/to/keyfile --raw\n" +
